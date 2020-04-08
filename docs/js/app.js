9282d53a2bd61b53deecb92e52954a65a8573d66
Yulric Sequeira <yulrics@gmail.com>
2020-04-08 10:47:26 -0400

    Fix for going to visualizations when on another page

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,16 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var vizDropdown = document.getElementById("viz-dropdown");
-    vizDropdown.addEventListener("click", function (e) {
-        e.preventDefault();
+// Store the current hash in the URL. If we are in the root page we will handle
+// scrolling to this location on the webpage
+var currentHash = window.location.hash;
+window.location.hash = "";
 
-        var NavbarHeight = 70;
-        window.scroll({
-            top:
-                document.querySelector(e.target.getAttribute("href")).offsetTop -
-                NavbarHeight,
-            left: 0,
-            behavior: 'smooth',
-        });
-        history.replaceState(null, null, document.location.pathname + e.target.getAttribute("href"));
-    })
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.pathname === "/") {
+        // We will handle all hash changes since we want to scroll to the
+        // visualizations ourselves
+        window.addEventListener("hashchange", function (e) {
+            e.preventDefault();
+        })
+
+        // If the user clicked on a has link on the root page outside of the 
+        // root page for eg. one of the visualizations dropdown links
+        if (currentHash) {
+            scrollToAnchorTag(currentHash)
+        }
+
+        var vizDropdown = document.getElementById("viz-dropdown");
+        vizDropdown.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            var anchorTag = e.target.getAttribute("href").replace("/", "")
+            scrollToAnchorTag(anchorTag)
+        })
+    }
 })
+
+function scrollToAnchorTag(anchorTag) {
+    var NavbarHeight = 70;
+    window.scroll({
+        top:
+            document.querySelector(anchorTag).offsetTop -
+            NavbarHeight,
+        left: 0,
+        behavior: 'smooth',
+    });
+    setTimeout(() => {
+        history.pushState(null, null, document.location.pathname + anchorTag);
+    })
+}
--- themes/hugo-lithium/static/js/app.js
+++ themes/hugo-lithium/static/js/app.js
@@ -1,16 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var vizDropdown = document.getElementById("viz-dropdown");
-    vizDropdown.addEventListener("click", function (e) {
-        e.preventDefault();
+console.log(window.location.hash)
+var hash = window.location.hash;
+window.location.hash = "";
 
-        var NavbarHeight = 70;
-        window.scroll({
-            top:
-                document.querySelector(e.target.getAttribute("href")).offsetTop -
-                NavbarHeight,
-            left: 0,
-            behavior: 'smooth',
-        });
-        history.replaceState(null, null, document.location.pathname + e.target.getAttribute("href"));
-    })
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.pathname === "/") {
+        window.addEventListener("hashchange", function (e) {
+            e.preventDefault();
+        })
+
+        if (hash) {
+            var NavbarHeight = 70;
+            window.scroll({
+                top:
+                    document.querySelector(hash).offsetTop -
+                    NavbarHeight,
+                left: 0,
+                behavior: 'smooth',
+            });
+
+            setTimeout(() => {
+                history.replaceState(null, null, document.location.pathname + hash);
+            })
+        }
+
+        var vizDropdown = document.getElementById("viz-dropdown");
+        vizDropdown.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            var NavbarHeight = 70;
+            window.scroll({
+                top:
+                    document.querySelector(e.target.getAttribute("href").replace("/", "")).offsetTop -
+                    NavbarHeight,
+                left: 0,
+                behavior: 'smooth',
+            });
+            history.pushState(null, null, document.location.pathname + e.target.getAttribute("href").replace("/", ""));
+        })
+    }
 })
