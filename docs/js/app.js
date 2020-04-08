ac134a9760ec1a995b82d5bbeec31fd3d76ce5c3
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-04-08 11:04:05 -0400

    [feature] build site

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,42 +1,41 @@
-// Store the current hash in the URL. If we are in the root page we will handle
-// scrolling to this location on the webpage
-var currentHash = window.location.hash;
+console.log(window.location.hash)
+var hash = window.location.hash;
 window.location.hash = "";
 
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.pathname === "/") {
-        // We will handle all hash changes since we want to scroll to the
-        // visualizations ourselves
         window.addEventListener("hashchange", function (e) {
             e.preventDefault();
         })
 
-        // If the user clicked on a has link on the root page outside of the 
-        // root page for eg. one of the visualizations dropdown links
-        if (currentHash) {
-            scrollToAnchorTag(currentHash)
+        if (hash) {
+            var NavbarHeight = 70;
+            window.scroll({
+                top:
+                    document.querySelector(hash).offsetTop -
+                    NavbarHeight,
+                left: 0,
+                behavior: 'smooth',
+            });
+
+            setTimeout(() => {
+                history.replaceState(null, null, document.location.pathname + hash);
+            })
         }
 
         var vizDropdown = document.getElementById("viz-dropdown");
         vizDropdown.addEventListener("click", function (e) {
             e.preventDefault();
 
-            var anchorTag = e.target.getAttribute("href").replace("/", "")
-            scrollToAnchorTag(anchorTag)
+            var NavbarHeight = 70;
+            window.scroll({
+                top:
+                    document.querySelector(e.target.getAttribute("href").replace("/", "")).offsetTop -
+                    NavbarHeight,
+                left: 0,
+                behavior: 'smooth',
+            });
+            history.pushState(null, null, document.location.pathname + e.target.getAttribute("href").replace("/", ""));
         })
     }
 })
-
-function scrollToAnchorTag(anchorTag) {
-    var NavbarHeight = 70;
-    window.scroll({
-        top:
-            document.querySelector(anchorTag).offsetTop -
-            NavbarHeight,
-        left: 0,
-        behavior: 'smooth',
-    });
-    setTimeout(() => {
-        history.pushState(null, null, document.location.pathname + anchorTag);
-    })
-}
