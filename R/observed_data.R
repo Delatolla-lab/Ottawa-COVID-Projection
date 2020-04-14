22622225c8dd5fd7cfde5f5860b3359aa200c0a3
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-04-14 13:32:15 -0400

    [Feature] add additional traces

--- R/observed_data.R
+++ R/observed_data.R
@@ -54,7 +54,41 @@ observed_figure <- function(data, title, y_label) {
     visible = TRUE, 
     orientation = "v"
   )
-  data <- list(trace1, trace2, trace3)
+  trace4 <- list(
+    line = list(
+      dash = "dash", 
+      color = "rgb(110, 199, 103)", 
+      width = 5
+    ), 
+    meta = list(columnNames = list(
+      x = "date", 
+      y = "previous 7 day"
+    )), 
+    mode = "lines", 
+    type = "scatter", 
+    xsrc = "wyusuf:11:3cac68", 
+    x = data$date,
+    ysrc = "wyusuf:11:3ea644", 
+    y = NA
+  )
+  trace5 <- list(
+    line = list(
+      dash = "dash", 
+      color = "rgb(110, 199, 103)", 
+      width = 5
+    ), 
+    meta = list(columnNames = list(
+      x = "date", 
+      y = "previous previous 7 day"
+    )), 
+    mode = "lines", 
+    type = "scatter", 
+    xsrc = "wyusuf:11:3cac68", 
+    x = data$date,
+    ysrc = "wyusuf:11:51678f", 
+    y = NA
+  )
+  data <- list(trace1, trace2, trace3, trace4, trace5)
   layout <- list(
     title = list(text = as.character(title)), 
     xaxis = list(
@@ -413,6 +447,8 @@ observed_figure <- function(data, title, y_label) {
   p <- add_trace(p, meta=trace1$meta, mode=trace1$mode, name=trace1$name, type=trace1$type, xsrc=trace1$xsrc, x=trace1$x, ysrc=trace1$ysrc, y=trace1$y, marker=trace1$marker, visible=trace1$visible, orientation=trace1$orientation)
   p <- add_trace(p, meta=trace2$meta, name=trace2$name, type=trace2$type, xsrc=trace2$xsrc, x=trace2$x, ysrc=trace2$ysrc, y=trace2$y, visible=trace2$visible, orientation=trace2$orientation)
   p <- add_trace(p, line=trace3$line, meta=trace3$meta, mode=trace3$mode, name=trace3$name, type=trace3$type, xsrc=trace3$xsrc, x=trace3$x, ysrc=trace3$ysrc, y=trace3$y, visible=trace3$visible, orientation=trace3$orientation)
+  p <- add_trace(p, line=trace4$line, meta=trace4$meta, mode=trace4$mode, type=trace4$type, xsrc=trace4$xsrc, x=trace4$x, ysrc=trace4$ysrc, y=trace4$y, y=trace4$y, visible=trace4$visible, orientation=trace4$orientation)
+  p <- add_trace(p, line=trace5$line, meta=trace5$meta, mode=trace5$mode, type=trace5$type, xsrc=trace5$xsrc, x=trace5$x, ysrc=trace5$ysrc, y=trace5$y, y=trace5$y, visible=trace5$visible, orientation=trace5$orientation)
   p <- layout(p, title=layout$title, xaxis=layout$xaxis, yaxis=layout$yaxis, barmode=layout$barmode, autosize=layout$autosize, template=layout$template, legend = list(x = 0.05, y = 0.9))
   p
 }
