2728eba99deb8304c7f3ce88abda9a89d1ddb041
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-04-17 11:52:24 -0400

    [Feature] complete script, implement script into rmd

--- R/effective_physical_distancing.R
+++ R/effective_physical_distancing.R
@@ -1,7 +1,20 @@
-source("../R/lag_replacement.R")
-source("../R/observed_data.R")
+# Load observed data
+tmp <- calc_expected_values_for_n_weeks(ott_observed, number_weeks = 1)
 
-## Effective physical distancing estimates
-observed_dt <- expected_values[[3]]$expected_val
-Tc <- 7 # mean generation time
-gamma <- 1/Tc
+# Baseline parameters
+baseline_dt <- 4
+Tc <- 7 
+gamma <- 1/Tc 
+g_baseline <- 2^(1/baseline_dt) - 1
+R_naught <- 1 + g_baseline*Tc
+beta_baseline <- R_naught*gamma
+
+
+# Observed parameters
+observed_dt <- as.numeric(tmp[[2]]) 
+g_observed <- 2^(1/observed_dt) - 1 
+Rt <- 1 + g_observed*Tc 
+beta_observed <- Rt*gamma 
+
+# Calculating effective physical distancing
+effective_pd <- (1 - ((g_observed + gamma)/beta_baseline)) * 100
