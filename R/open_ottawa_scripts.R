95aa19114ff514f283fe6ae1e1fce3c048e9f1ed
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-09-24 14:24:25 -0400

    [Bug] fix dataframe creation issues, update rmds, upload new observed data csv

--- R/open_ottawa_scripts.R
+++ R/open_ottawa_scripts.R
@@ -1,9 +1,3 @@
-# Pull JSON objects
-ottawa_case_data <-
-  fromJSON("https://opendata.arcgis.com/datasets/6bfe7832017546e5b30c5cc6a201091b_0/FeatureServer/0/query?where=1%3D1&outFields=_Date,Cumulative_Active_Cases_by_Episode_Date,Cumulative_Deaths_by_Date_of_Death,7-day_Average_of_Newly_Reported_cases_by_Reported_Date,Cases_Newly_Admitted_to_Hospital,Cases_Currently_in_Hospital,Cases_Currently_in_ICU,Daily_Cases_by_Episode_Date,Daily_Cases_by_Reported_Date&outSR=4326&f=json")
-ottawa_test_data <-
-  fromJSON("https://opendata.arcgis.com/datasets/26c902bf1da44d3d90b099392b544b81_0/FeatureServer/0/query?where=1%3D1&outFields=_Date,Daily_%_Positivity&outSR=4326&f=json")
-
 # Clean and merge JSON objects as data frame
 data_creation <- function(ottawa_case_data, ottawa_test_data){
   ottawa_test <- ottawa_test_data[[8]][[1]] %>%
@@ -37,9 +31,4 @@ data_creation <- function(ottawa_case_data, ottawa_test_data){
     filter(date >= "2020-03-17") %>%
     full_join(ottawa_test, by = "date")
   
-  # Save as csv file
-  write.csv(ottawa_data,
-        "../../Data/Observed data/Ottawa_Observed_COVID_Hospital_Use_test.csv")
 }
-  
-
