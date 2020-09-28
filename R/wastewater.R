8a54522ce763548b5fd1b5bcb32793136ed905b2
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-09-28 09:29:15 -0400

    [Feature] update wastewater data, update wastewater prep script

--- R/wastewater.R
+++ R/wastewater.R
@@ -1,20 +1,16 @@
 # Script to clean wastewater data
 wastewater_prep <- function(data){
-  # Drop blank rows & columns
-  data <- data[-c(1:4),-11]
-  # Set appropriate column headers
-  colnames(data) <- as.character(unlist(data[1,]))
-  data <- data[-1,]
   # Rename gene headers 
-  names(data)[3] <- "N1"
-  names(data)[7] <- "N2"
-  names(data)[10] <- "N1_N2"
-  names(data)[11] <- "rolling_avg"
-  # Remove redundant columns
-  data <- data[,-c(2, 4:6, 8:9, 12)]
+  names(data)[1] <- "date"
+  names(data)[2] <- "N1"
+  names(data)[3] <- "N1_stdev"
+  names(data)[4] <- "N2"
+  names(data)[5] <- "N2_stdev"
+  names(data)[6] <- "N1_N2"
+  names(data)[7] <- "rolling_avg"
+  names(data)[8] <- "pct_change"
   # Remove rows beyond current date
   data %>%
-    rename(date = "Sample Date") %>%
     mutate(date = as.Date(parse_date_time(date, c('dm', 'dmy')))) %>%
     filter(!is.na(date))
 }
