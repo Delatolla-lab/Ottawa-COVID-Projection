1d6023f70d4ab0ca8b3263d028d85822eae682e8
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-10-06 15:21:50 -0400

    [Feature] create rolling averages for viral signal, new cases, active cases

--- R/wastewater.R
+++ R/wastewater.R
@@ -14,12 +14,40 @@ wastewater_prep <- function(data){
   # Remove rows beyond current date
   data %>%
     mutate(date = as.Date(parse_date_time(date, c('dm', 'dmy')))) %>%
-    filter(!is.na(date))
+    filter(!is.na(date)) %>%
+    # create daily rate of change of viral signal
+    mutate(
+      viral_roc_daily = ((N1_N2_avg - lag(N1_N2_avg))/lag(N1_N2_avg))/
+        (as.numeric(as_date(date)-lag(as_date(date))))
+    )
 }
 
 # Script to merge wastewater data into covid data
 merge_data <- function(data1, data2){
   data1 %>%
     full_join(data2, by = "date") %>%
-    filter(date >= "2020-04-08")
+    filter(date >= "2020-04-08") %>%
+    # create 5 day rolling avg of daily rate of change of viral signal
+    mutate(
+      avg_viral_roc_5_day =
+        zoo::rollapply(viral_roc_daily, width=5,
+                       FUN=function(x) mean(x, na.rm=TRUE),
+                       by=1, by.column=TRUE, partial=TRUE,
+                       fill=NA, align="right")
+    ) %>%
+    # create 5 day rolling avg of reported new cases & active cases
+    mutate(
+      observed_new_cases_5_day =
+        zoo::rollapply(
+          observed_new_cases, width=5,
+          FUN=function(x) mean(x, na.rm=TRUE),
+          by=1, by.column=TRUE, partial=TRUE,
+          fill=NA, align="right"),
+      observed_active_cases_5_day =
+        zoo::rollapply(
+          observed_active_cases, width=5,
+          FUN=function(x) mean(x, na.rm=TRUE),
+          by=1, by.column=TRUE, partial=TRUE,
+          fill=NA, align="right"),
+    )
 }
