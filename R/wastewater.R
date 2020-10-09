3531473f9df94755bd140ebcca4576d947c9ee1e
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-10-09 11:57:46 -0400

    [Refactor] modify cleaning of ww data, add 7 day avgs to figures

--- R/wastewater.R
+++ R/wastewater.R
@@ -2,19 +2,16 @@
 wastewater_prep <- function(data){
   # Rename gene headers 
   names(data)[1] <- "date"
-  names(data)[2] <- "N1"
-  names(data)[3] <- "N1_stdev"
-  names(data)[5] <- "N2"
-  names(data)[6] <- "N2_stdev"
-  names(data)[7] <- "N1_N2_avg"
-  names(data)[8] <- "rolling_avg"
-  names(data)[9] <- "pct_change"
-  # Remove second date header
-  data <- data[,-4]
-  # Remove rows beyond current date
+  names(data)[5] <- "N1"
+  names(data)[7] <- "N2"
+  
   data %>%
+    select(date, N1, N2) %>%
+    # Remove rows beyond current date
     mutate(date = as.Date(parse_date_time(date, c('dm', 'dmy')))) %>%
     filter(!is.na(date)) %>%
+    # Create mean value of N1 and N2
+    mutate(N1_N2_avg = (N1 + N2)/2) %>%
     # create daily rate of change of viral signal
     mutate(
       viral_roc_daily = ((N1_N2_avg - lag(N1_N2_avg))/lag(N1_N2_avg))/
@@ -33,7 +30,15 @@ merge_data <- function(data1, data2){
         rollapply(N1_N2_avg, width=5,
                   FUN=function(x) mean(x, na.rm=TRUE),
                   by=1, by.column=TRUE, partial=TRUE,
-                  fill=NA, align="center")
+                  fill=NA, align="right")
+    ) %>%
+    # create 7 day rolling avg of viral signal
+    mutate(
+      N1_N2_7_day =
+        rollapply(N1_N2_avg, width=7,
+                  FUN=function(x) mean(x, na.rm=TRUE),
+                  by=1, by.column=TRUE, partial=TRUE,
+                  fill=NA, align="right")
     ) %>%
     # create 10 day rolling avg of viral signal
     mutate(
@@ -41,7 +46,7 @@ merge_data <- function(data1, data2){
         rollapply(N1_N2_avg, width=10,
                   FUN=function(x) mean(x, na.rm=TRUE),
                   by=1, by.column=TRUE, partial=TRUE,
-                  fill=NA, align="center")
+                  fill=NA, align="right")
     ) %>%
     # create 10 day rolling avg of daily rate of change of viral signal
     mutate(
@@ -49,7 +54,7 @@ merge_data <- function(data1, data2){
         rollapply(viral_roc_daily, width=10,
                   FUN=function(x) mean(x, na.rm=TRUE),
                   by=1, by.column=TRUE, partial=TRUE,
-                  fill=NA, align="center")
+                  fill=NA, align="right")
     ) %>%
     # create change in 5 day rolling avg from 10 day rolling avg 10 days ago
     mutate(
@@ -61,11 +66,23 @@ merge_data <- function(data1, data2){
       change_N1_N2_10_day =
         (N1_N2_10_day - lag(N1_N2_10_day, 10))/lag(N1_N2_10_day, 10) * 100
     ) %>%
-    # create 5 day rolling avg of reported new cases & active cases
+    # create 5, 7, 10 day rolling avgs of reported new cases & active cases
     mutate(
       observed_new_cases_5_day =
         rollapply(
           observed_new_cases, width=5,
+          FUN=function(x) mean(x, na.rm=TRUE),
+          by=1, by.column=TRUE, partial=TRUE,
+          fill=NA, align="right"),
+      observed_new_cases_7_day =
+        rollapply(
+          observed_new_cases, width=7,
+          FUN=function(x) mean(x, na.rm=TRUE),
+          by=1, by.column=TRUE, partial=TRUE,
+          fill=NA, align="right"),
+      observed_new_cases_10_day =
+        rollapply(
+          observed_new_cases, width=10,
           FUN=function(x) mean(x, na.rm=TRUE),
           by=1, by.column=TRUE, partial=TRUE,
           fill=NA, align="right"),
@@ -75,9 +92,15 @@ merge_data <- function(data1, data2){
           FUN=function(x) mean(x, na.rm=TRUE),
           by=1, by.column=TRUE, partial=TRUE,
           fill=NA, align="right"),
-      observed_new_cases_10_day =
+      observed_active_cases_7_day =
         rollapply(
-          observed_new_cases, width=10,
+          observed_active_cases, width=7,
+          FUN=function(x) mean(x, na.rm=TRUE),
+          by=1, by.column=TRUE, partial=TRUE,
+          fill=NA, align="right"),
+      observed_active_cases_10_day =
+        rollapply(
+          observed_active_cases, width=10,
           FUN=function(x) mean(x, na.rm=TRUE),
           by=1, by.column=TRUE, partial=TRUE,
           fill=NA, align="right")
