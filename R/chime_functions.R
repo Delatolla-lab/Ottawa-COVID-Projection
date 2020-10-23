842a40c12afaaa1fa072a74f7dcf1b86e71b9ecb
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-10-23 08:10:46 -0400

    [Feature] function to process hosp data for sim

---
+++ R/chime_functions.R
@@ -0,0 +1,16 @@
+# Functions to pull hospitalization data from Open Ottawa's API to be used
+# in the CHIME-SIMS simulation
+
+## Import open Ottawa hospitalization data & export to Python folder for
+## simulation
+hosp_export <- function(json_address, dest){
+  object <- fromJSON(json_address)
+  data <- data_extract(object) %>%
+    rename(
+      date = "_Date",
+      hosp = "Cases_Currently_in_Hospital"
+    )
+  data$vent <- NULL
+  data$mort <- NULL
+  write.csv(data, dest)
+}
