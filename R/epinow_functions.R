a51953efd2ca3b66fb491da229cf0e80cd09c6f6
wyusuf068 <wyusu068@uottawa.ca>
2021-04-15 10:26:36 -0400

    [Test] use rt_opt to calc proj

--- R/epinow_functions.R
+++ R/epinow_functions.R
@@ -25,9 +25,9 @@ short_term_forecast <- function(data,
       EpiNow2::epinow(reported_cases = data_formatted, 
                       generation_time = generation_time,
                       delays = delay_opts(incubation_period, reporting_delay),
-                      rt = NULL,
+                      rt = rt_opts(prior = list(mean =2, sd = 0.2), rw = 7),
                       stan = stan_opts(cores = 4),
-                      horizon = 14)
+                      gp = NULL, horizon = 14)
   # Extract output
   if(output == as.character("projections")){
     forecast <-
