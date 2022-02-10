345f247419e98cc625669536ebc1d5f214c94739
Warsame Yusuf <wyusu068@Uottawa.ca>
2022-02-10 14:33:26 -0500

    [Refactor] rename historic forecast file, update 7/14 day hosp script

--- ww_projections_development/R/hospital_7_and_14_days_forecast.R
+++ ww_projections_development/R/hospital_7_and_14_days_forecast.R
@@ -22,44 +22,44 @@ generation_time <-
 incubation_period <-
   get_incubation_period(disease = "SARS-CoV-2", source = "lauer")
 
-end_date = as.Date("2021-01-31")
-delay_intervals <- c(4, 10, 14)
+start_date <- as.Date("2021-07-20")
+end_date = as.Date("2021-12-20")
 hosp_data_forecast <- list()
-for (delay in delay_intervals){
-  x <- 0
-  while(end_date <= as.Date("2022-01-20")){
-    x <- x + 1
-    hosp_forecast <- short_term_forecast(
-      data = ott_covid_data,
-      input = "observed_census_ICU_p_acute_care",
-      start_date = as.Date("2021-07-20") + 1,
-      end_date = as.Date("2021-12-20") + 1,
-      input_multiplier = 1,
-      omit_last_date = TRUE,
-      generation_time = generation_time,
-      incubation_period = incubation_period,
-      reporting_delay = bootstrapped_dist_fit(rlnorm(100, log(delay), 1), max_value = 30),
-      output = "projections"
-    ) %>%
-      filter(variable == "reported_cases",
-             type == "forecast")
-    
-    ott_data <- ott_covid_data %>%
-      mutate(date = as.Date(date)) %>%
-      select(date, observed_census_ICU_p_acute_care)
-    hosp_data_forecast[[paste0("reporting_delay_",delay)]][[x]] <- ott_data %>%
-      full_join(hosp_forecast) %>%
-      filter(duplicated(date) == FALSE,
-             date > first(date),
-             !is.na(variable)) %>%
-      arrange(date)
-  }
+x <- 0
+while(end_date <= as.Date("2022-01-20")){
+  x <- x + 1
+  hosp_forecast <- short_term_forecast(
+    data = ott_covid_data,
+    input = "observed_census_ICU_p_acute_care",
+    start_date = start_date,
+    end_date = end_date,
+    input_multiplier = 1,
+    omit_last_date = TRUE,
+    generation_time = generation_time,
+    incubation_period = incubation_period,
+    reporting_delay = bootstrapped_dist_fit(rlnorm(100, log(4), 1), max_value = 30),
+    output = "projections"
+  ) %>%
+    filter(variable == "reported_cases",
+           type == "forecast")
+  
+  ott_data <- ott_covid_data %>%
+    mutate(date = as.Date(date)) %>%
+    select(date, observed_census_ICU_p_acute_care)
+  hosp_data_forecast[[x]] <- ott_data %>%
+    full_join(hosp_forecast) %>%
+    filter(duplicated(date) == FALSE,
+           date > first(date),
+           !is.na(variable)) %>%
+    arrange(date)
+  
+  start_date <- start_date + 1
+  end_date <- end_date + 1
 }
 
-hosp_forecast_7_14_days = list()
-for (delay in delay_intervals){
-  hosp_forecast_7_14_days[[paste0("reporting_delay_",delay)]] = create_7_14_days_forecast(hosp_data_forecast[[paste0("reporting_delay_",delay)]])
-}
+hosp_data_forecast_7_14_days <-
+  historic_forecast_dataset(hosp_data_forecast, 
+                            obs_column = "observed_census_ICU_p_acute_care")
 
-
-save(hosp_data_forecast, file = "ww_projections_development/Data/ottawa_historic_hosp_proj.RData")
+save(hosp_data_forecast_7_14_days,
+     file = "ww_projections_development/Data/ottawa_historic_hosp_proj_7_14_days.RData")
