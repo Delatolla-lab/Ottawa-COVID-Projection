d06da1915b79cb082a2311ee8d0955ed90a11527
Warsame Yusuf <wyusu068@Uottawa.ca>
2022-02-10 14:30:09 -0500

    [Refactor] add parameters to soft code function

--- R/create_7_14_days_forecast.R
+++ R/create_7_14_days_forecast.R
@@ -1,23 +1,29 @@
-create_7_14_days_forecast <- function(data){
-  median_7_vec = c()
-  median_14_vec = c()
-  observed_7_vec = c()
-  observed_14_vec = c()
-  for (x in length(data)){
-    for(i in 1:nrow(data[[x]])) {  
-      # for-loop over rows
-        if (i == 7){
-          median_7_vec <- c(median_7_vec, data[[x]][i, median])
-          observed_7_vec <- c(observed_7_vec, data[[x]][i, observed_census_ICU_p_acute_care])
-        }
-        else if (i == 14){
-          median_14_vec <- c(median_14_vec, data[[x]][i, median])
-          observed_14_vec <- c(observed_14_vec, data[[x]][i, observed_census_ICU_p_acute_care])
-        }
-      }
-  }
-  forecast_7_14_data = data.frame("7_days_forecast" = median_7_vec, "14_days_forecast" = median_14_vec,
-                                  "observed_7_days" = observed_7_vec, "observed_14_days" = observed_14_vec)
-  return(forecast_7_14_data)
+# This function creates a dataset that pulls data from historic projections
+# and combines it with observed data. Function defaults to pull 7th and 14th day
+# projections, but users can select other dates. Function defaults to median as
+# projection variable of interest, but users can change to mean. 
+
+create_historic_forecast_dataset <- function(data,
+                                      days = c(7, 14),
+                                      projection_variable = "median",
+                                      ci = 90,
+                                      obs_column){
   
+  for (x in 1:length(data)){
+    data[[x]] <- data[[x]] %>%
+      select(date, projection_variable, obs_column, sd, paste0("lower_", ci),
+             paste0("upper_", ci))
+    daily_proj <- list()
+    i <- 0
+    for(day in unique(days)){
+      i <- i + 1
+      daily_proj[[i]] <- data[[x]][day,] %>%
+        rename_with(~paste0(.,paste0("_day_", day)), .cols = -c("date", obs_column))
+    }
+    data[[x]] <- bind_rows(daily_proj)
+  }
+  forecast_dataset <- bind_rows(data) %>%
+    arrange(date) %>%
+    relocate(date, obs_column)
+  return(forecast_dataset)
 }
